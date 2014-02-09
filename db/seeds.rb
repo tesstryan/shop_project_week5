17995838a07f359803bcc186e126d5fb721b9929
tesstryan <tesstryan@gmail.com>
2014-02-09 17:29:12 -0600

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -74,7 +74,7 @@ Product.create(
     description: 'Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.',
     image: 'batshelf.jpg')
   Product.create(
-    title: 'Paul Bunyan Beard',
+    title: 'Paul Bunyan Bee Beard',
     price: '15',
     description: 'Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.',
     image: 'bees.jpg')
